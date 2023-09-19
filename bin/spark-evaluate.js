acc71cb437f42780ca276e0d0d9e8478a0578139
Miroslav Bajtoš <oss@bajtos.net>
2023-09-19 10:07:01 +0200

    feat: setup Sentry integration for errors (#8)
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/spark-evaluate.js
+++ bin/spark-evaluate.js
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/node'
 import { startEvaluate } from '../index.js'
 import assert from 'node:assert'
 import { ethers } from 'ethers'
@@ -7,11 +8,19 @@ import { Web3Storage } from 'web3.storage'
 import fs from 'node:fs/promises'
 
 const {
+  SENTRY_ENVIRONMMENT = 'development',
   IE_CONTRACT_ADDRESS = '0x816830a1e536784ecb37cf97dfd7a98a82c86643',
   RPC_URL = 'https://api.calibration.node.glif.io/rpc/v0',
   WALLET_SEED,
   WEB3_STORAGE_API_TOKEN
 } = process.env
+
+Sentry.init({
+  dsn: 'https://d0651617f9690c7e9421ab9c949d67a4@o1408530.ingest.sentry.io/4505906069766144',
+  environment: SENTRY_ENVIRONMMENT,
+  // Performance Monitoring
+  tracesSampleRate: 0.1 // Capture 10% of the transactions
+})
 
 assert(WALLET_SEED, 'WALLET_SEED required')
 assert(WEB3_STORAGE_API_TOKEN, 'WEB3_STORAGE_API_TOKEN required')
