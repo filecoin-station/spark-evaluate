17fb0fdba855fb0cb7508e0421a50487a8db672f
Miroslav Bajtoš <oss@bajtos.net>
2023-10-25 16:45:56 +0200

    feat: reward one participant per inet group
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- lib/typings.d.ts
+++ lib/typings.d.ts
@@ -21,3 +21,24 @@ export type RecordTelemetryFn = (
   name: string,
   fn: (point: Point) => void
 ) => void
+
+export type FraudAssesment =
+  | 'OK'
+  | 'INVALID_TASK'
+  | 'NO_INET_GROUP'
+  | 'DUPLICATE_SUBNET'
+
+export interface Measurement {
+  participantAddress: string;
+  fraudAssessment?: FraudAssesment;
+
+  cid: string;
+  provider_address: string;
+  protocol: string;
+  inet_group: string;
+
+  start_at: string;
+  first_byte_at: string;
+  end_at: string;
+  finished_at: string;
+}
