d8f441117bb5256090a41ed12d63f1b4bf0763f3
Miroslav Bajtoš <oss@bajtos.net>
2023-11-22 17:09:25 +0100

    fixup!
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/spark-api.js
+++ test/spark-api.js
@@ -7,25 +7,25 @@ describe('spark-api client', () => {
   it('fetches round details', async () => {
     const { retrievalTasks, ...details } = await fetchRoundDetails(
       '0xaaef78eaf86dcf34f275288752e892424dda9341',
-      410,
+      407,
       recordTelemetry
     )
 
     assert.deepStrictEqual(details, {
-      roundId: '3408' // BigInt serialized as String
+      roundId: '3405' // BigInt serialized as String
     })
 
-    assert.strictEqual(retrievalTasks.length, 80)
+    assert.strictEqual(retrievalTasks.length, 400)
     assert.deepStrictEqual(retrievalTasks.slice(0, 2), [
       {
-        cid: 'QmQmAT34YQZUXWPonEBLYayGPqbfKT7WNVKyejYb8BfbJ8',
+        cid: 'Qmcieq8Lf1r4qfifJkKpdpVzajdsbSGZmBiL3vy6syWo5T',
         protocol: 'graphsync',
-        providerAddress: '/ip4/115.42.169.232/tcp/34568/p2p/12D3KooWH6ZS7VtsG8FWhxopQVSkvjMSbKtdiDgdfx7DR2oKopzV'
+        providerAddress: '/ip4/210.209.69.37/tcp/34568/p2p/12D3KooWSHG9vVStHMi9vhfgD1XaR221Ur6RjWcVsiY7WfVaX4QL'
       },
       {
-        cid: 'bafybeift5kk74nwk6mezbicri23a5yvybdu63ege3c3jexxozfvhdd37s4',
+        cid: 'QmejPgwMo5jzJDsArpGCn9Tz1gSQNXYU722pFe2kcLHzpq',
         protocol: 'graphsync',
-        providerAddress: '/ip4/103.145.73.12/tcp/41727/p2p/12D3KooWNo9msgn3T1uhmgxCjg5YKghMEHdNGr8uU4EtFC3a9zev'
+        providerAddress: '/ip4/138.113.222.200/tcp/19013/p2p/12D3KooWCnJeMnur6ScjXXUo8ptMKac88fDbD8GELmdDrdg3FBk5'
       }
     ])
   })
