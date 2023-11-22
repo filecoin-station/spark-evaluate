e1b940d92ee5cd8edb58f8eb7a0ff16d5a8bc13b
Miroslav Bajtoš <oss@bajtos.net>
2023-11-22 15:58:16 +0100

    test: update spark-api to use a more recent round
    The endpoint `/rounds/meridian/contract/index` does not support
    historic rounds.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/spark-api.js
+++ test/spark-api.js
@@ -6,26 +6,26 @@ const recordTelemetry = (measurementName, fn) => { /* no-op */ }
 describe('spark-api client', () => {
   it('fetches round details', async () => {
     const { retrievalTasks, ...details } = await fetchRoundDetails(
-      '0x381b50e8062757c404dec2bfae4da1b495341af9',
-      10,
+      '0xaaef78eaf86dcf34f275288752e892424dda9341',
+      410,
       recordTelemetry
     )
 
     assert.deepStrictEqual(details, {
-      roundId: '995' // BigInt serialized as String
+      roundId: '3408' // BigInt serialized as String
     })
 
-    assert.strictEqual(retrievalTasks.length, 30)
+    assert.strictEqual(retrievalTasks.length, 80)
     assert.deepStrictEqual(retrievalTasks.slice(0, 2), [
       {
-        cid: 'QmUen1QdStwcqYaRJKW59GfEk1y8TyqyrvisZ5Mm2ZwMhU',
-        protocol: 'bitswap',
-        providerAddress: '/dns4/elastic.dag.house/tcp/443/wss/p2p/QmQzqxhK82kAmKvARFZSkUVS6fo9sySaiogAnx5EnZ6ZmC'
+        cid: 'QmQmAT34YQZUXWPonEBLYayGPqbfKT7WNVKyejYb8BfbJ8',
+        protocol: 'graphsync',
+        providerAddress: '/ip4/115.42.169.232/tcp/34568/p2p/12D3KooWH6ZS7VtsG8FWhxopQVSkvjMSbKtdiDgdfx7DR2oKopzV'
       },
       {
-        cid: 'bafybeidijvhvuvizg2ofeqos7l2kd4uanbawzx6qazyko5kbvth6dlhn5e',
-        protocol: 'bitswap',
-        providerAddress: '/dns4/elastic.dag.house/tcp/443/wss/p2p/QmQzqxhK82kAmKvARFZSkUVS6fo9sySaiogAnx5EnZ6ZmC'
+        cid: 'bafybeift5kk74nwk6mezbicri23a5yvybdu63ege3c3jexxozfvhdd37s4',
+        protocol: 'graphsync',
+        providerAddress: '/ip4/103.145.73.12/tcp/41727/p2p/12D3KooWNo9msgn3T1uhmgxCjg5YKghMEHdNGr8uU4EtFC3a9zev'
       }
     ])
   })
