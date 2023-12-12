027c390fe7213cb6e2d672730572844af987ace3
Miroslav Bajtoš <oss@bajtos.net>
2023-12-12 15:16:13 +0100

    feat: record subnets/participants/instances per task (#97)
    Help us understand how many data points we have for each task 
    (what is the effective committee size). Are the committees always
    large enough to give us enough confidence in finding an honest majority?
    
    Also, record how many measurements (retrieval checks) we provide for
    each task to understand how much load we put on SPs.
    
    InfluxDB point name: `committees`
    Field prefixes:
    - `subnets_`
    - `participants_`
    - `instances_`
    - `measurements_` (all measurements, independent of fraud assessment)
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/retrieval-stats.test.js
+++ test/retrieval-stats.test.js
@@ -1,7 +1,11 @@
 import assert from 'node:assert'
 import createDebug from 'debug'
 import { Point } from '../lib/telemetry.js'
-import { buildRetrievalStats, getValueAtPercentile } from '../lib/retrieval-stats.js'
+import {
+  buildRetrievalStats,
+  getValueAtPercentile,
+  recordCommitteeSizes
+} from '../lib/retrieval-stats.js'
 import { VALID_MEASUREMENT } from './helpers/test-data.js'
 import { assertPointFieldValue } from './helpers/assertions.js'
 
@@ -119,5 +123,161 @@ describe('getValueAtPercentile', () => {
       getValueAtPercentile([10, 20, 30], 90),
       28
     )
+  })
+})
+
+describe('recordCommitteeSizes', () => {
+  it('reports unique subnets', async () => {
+    const measurements = [
+      // task 1
+      {
+        ...VALID_MEASUREMENT,
+        inet_group: 'ig1'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        participantAddress: '0xanother',
+        // duplicate measurement in the same subnet, should be ignored
+        inet_group: 'ig1'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        inet_group: 'ig2'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        inet_group: 'ig3'
+      },
+
+      // task 2
+      {
+        ...VALID_MEASUREMENT,
+        cid: 'bafyanother'
+      }
+
+    ]
+
+    const point = new Point('committees')
+    recordCommitteeSizes(measurements, point)
+    debug(point.name, point.fields)
+
+    assertPointFieldValue(point, 'subnets_min', '1i')
+    assertPointFieldValue(point, 'subnets_mean', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'subnets_p50', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'subnets_max', '3i')
+  })
+
+  it('reports unique participants', async () => {
+    const measurements = [
+      // task 1
+      {
+        ...VALID_MEASUREMENT,
+        participantAddress: '0xone'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        inet_group: 'ig1',
+        // duplicate measurement by the same participant, should be ignored
+        participantAddress: '0xone'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        participantAddress: '0xtwo'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        participantAddress: '0xthree'
+      },
+
+      // task 2
+      {
+        ...VALID_MEASUREMENT,
+        cid: 'bafyanother'
+      }
+
+    ]
+
+    const point = new Point('committees')
+    recordCommitteeSizes(measurements, point)
+    debug(point.name, point.fields)
+
+    assertPointFieldValue(point, 'participants_min', '1i')
+    assertPointFieldValue(point, 'participants_mean', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'participants_p50', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'participants_max', '3i')
+  })
+
+  it('reports unique instances', async () => {
+    const measurements = [
+      // task 1
+      {
+        ...VALID_MEASUREMENT,
+        inet_group: 'ig1',
+        participantAddress: '0xone'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        // duplicate measurement by the same participant in the same subnet, should be ignored
+        inet_group: 'ig1',
+        participantAddress: '0xone'
+      },
+      {
+        ...VALID_MEASUREMENT,
+        // same participant address but different subnet
+        inet_group: 'ig2',
+        participantAddress: '0xone'
+      },
+      {
+        ...VALID_MEASUREMENT
+      },
+
+      // task 2
+      {
+        ...VALID_MEASUREMENT,
+        cid: 'bafyanother'
+      }
+
+    ]
+
+    const point = new Point('committees')
+    recordCommitteeSizes(measurements, point)
+    debug(point.name, point.fields)
+
+    assertPointFieldValue(point, 'instances_min', '1i')
+    assertPointFieldValue(point, 'instances_mean', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'instances_p50', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'instances_max', '3i')
+  })
+
+  it('reports number of all measurements', async () => {
+    const measurements = [
+      // task 1
+      {
+        ...VALID_MEASUREMENT
+      },
+      {
+        ...VALID_MEASUREMENT
+
+      },
+      {
+        ...VALID_MEASUREMENT
+      },
+
+      // task 2
+      {
+        ...VALID_MEASUREMENT,
+        cid: 'bafyanother'
+      }
+
+    ]
+
+    const point = new Point('committees')
+    recordCommitteeSizes(measurements, point)
+    debug(point.name, point.fields)
+
+    assertPointFieldValue(point, 'measurements_min', '1i')
+    assertPointFieldValue(point, 'measurements_mean', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'measurements_p50', '2i') // (3+1)/2 rounded down
+    assertPointFieldValue(point, 'measurements_max', '3i')
   })
 })
