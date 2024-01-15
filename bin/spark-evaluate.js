8f1e16602d6824fcffad044ee0d0082bdbed61ae
Julian Gruber <julian@juliangruber.com>
2024-01-15 14:04:30 +0100

    fly: remove `services` section

--- bin/spark-evaluate.js
+++ bin/spark-evaluate.js
@@ -9,7 +9,6 @@ import { newDelegatedEthAddress } from '@glif/filecoin-address'
 import { recordTelemetry } from '../lib/telemetry.js'
 import fs from 'node:fs/promises'
 import { fetchMeasurements } from '../lib/preprocess.js'
-import http from 'node:http'
 
 const {
   SENTRY_ENVIRONMENT = 'development',
@@ -46,9 +45,6 @@ const ieContract = new ethers.Contract(
   provider
 )
 const ieContractWithSigner = ieContract.connect(signer)
-
-// FIXME fly.io
-http.createServer((_, res) => res.end()).listen(8080)
 
 startEvaluate({
   ieContract,
