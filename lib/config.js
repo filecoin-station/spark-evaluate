filecoin-station/spark-evaluate

commit d495910ad389b38cb36237756b0e8797f65d2dba

FIXME disable api.chain.love

lib/config.js
@@ -1,6 +1,9 @@
 const {
   IE_CONTRACT_ADDRESS = '0x811765AccE724cD5582984cb35f5dE02d587CA12',
-  RPC_URLS = 'https://api.node.glif.io/rpc/v0,https://api.chain.love/rpc/v1',
+  // FIXME Add back chain.love either when it's online or once onContractEvent
+  // supports rpc failover
+  // RPC_URLS = 'https://api.node.glif.io/rpc/v0,https://api.chain.love/rpc/v1',
+  RPC_URLS = 'https://api.node.glif.io/rpc/v0'
   GLIF_TOKEN,
   DATABASE_URL = 'postgres://localhost:5432/spark_stats',
   SPARK_API = 'https://api.filspark.com'

commit 44b12836a6477f5de7c215bb409923e5a7a641bf

fix: syntax error introduced by d495910
Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

lib/config.js
@@ -3,7 +3,7 @@ const {
   // FIXME Add back chain.love either when it's online or once onContractEvent
   // supports rpc failover
   // RPC_URLS = 'https://api.node.glif.io/rpc/v0,https://api.chain.love/rpc/v1',
-  RPC_URLS = 'https://api.node.glif.io/rpc/v0'
+  RPC_URLS = 'https://api.node.glif.io/rpc/v0',
   GLIF_TOKEN,
   DATABASE_URL = 'postgres://localhost:5432/spark_stats',
   SPARK_API = 'https://api.filspark.com'