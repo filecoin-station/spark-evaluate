43e491768b62a694e72e2f7d9bbf2c44ab04687c
Miroslav Bajtoš <oss@bajtos.net>
2024-01-17 10:08:34 +0100

    fixup! comment
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- migrations/002.do.delete-invalid-rsr-data.sql
+++ migrations/002.do.delete-invalid-rsr-data.sql
@@ -1,4 +1,4 @@
 -- We had a bug in the code updating retrieval stats.
--- As a result, existing data are meaningless and misleading.
+-- As a result, existing data is meaningless and misleading.
 -- Let's delete it.
 DELETE FROM retrieval_stats;
