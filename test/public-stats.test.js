7a2d14d11f10513e2946eba863f4721383ed322f
Miroslav Bajtoš <oss@bajtos.net>
2024-01-18 09:53:02 +0100

    fixup! add tests + optimise performance
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/public-stats.test.js
+++ test/public-stats.test.js
@@ -4,7 +4,8 @@ import pg from 'pg'
 import { DATABASE_URL } from '../lib/config.js'
 import { migrateWithPgClient } from '../lib/migrate.js'
 import { VALID_MEASUREMENT } from './helpers/test-data.js'
-import { updatePublicStats } from '../lib/public-stats.js'
+import { mapParticipantsToIds, updateDailyParticipants, updatePublicStats } from '../lib/public-stats.js'
+import { beforeEach } from 'mocha'
 
 const createPgClient = async () => {
   const pgClient = new pg.Client({ connectionString: DATABASE_URL })
@@ -19,12 +20,19 @@ describe('public-stats', () => {
     await migrateWithPgClient(pgClient)
   })
 
+  let today
   beforeEach(async () => {
     await pgClient.query('DELETE FROM retrieval_stats')
+    await pgClient.query('DELETE FROM daily_participants')
+    // empty `participants` table in such way that the next participants.id will be always 1
+    await pgClient.query('TRUNCATE TABLE participants RESTART IDENTITY CASCADE')
+
     // Run all tests inside a transaction to ensure `now()` always returns the same value
     // See https://dba.stackexchange.com/a/63549/125312
     // This avoids subtle race conditions when the tests are executed around midnight.
     await pgClient.query('BEGIN TRANSACTION')
+
+    today = await getCurrentDate()
   })
 
   afterEach(async () => {
@@ -37,8 +45,6 @@ describe('public-stats', () => {
 
   describe('retrieval_stats', () => {
     it('creates or updates the row for today', async () => {
-      const { rows: [{ today }] } = await pgClient.query('SELECT now()::DATE::TEXT as today')
-
       /** @type {import('../lib/preprocess').Measurement[]} */
       const honestMeasurements = [
         { ...VALID_MEASUREMENT, retrievalResult: 'OK' },
@@ -64,4 +70,74 @@ describe('public-stats', () => {
       ])
     })
   })
+
+  describe('daily_participants', () => {
+    it('submits daily_participants data for today', async () => {
+      /** @type {import('../lib/preprocess').Measurement[]} */
+      const honestMeasurements = [
+        { ...VALID_MEASUREMENT, participantAddress: '0x10' },
+        { ...VALID_MEASUREMENT, participantAddress: '0x10' },
+        { ...VALID_MEASUREMENT, participantAddress: '0x20' }
+      ]
+      await updatePublicStats({ createPgClient, honestMeasurements })
+
+      const { rows } = await pgClient.query(
+        'SELECT day::TEXT, participant_id FROM daily_participants'
+      )
+      assert.deepStrictEqual(rows, [
+        { day: today, participant_id: 1 },
+        { day: today, participant_id: 2 }
+      ])
+    })
+
+    it('creates a new daily_participants row', async () => {
+      await updateDailyParticipants(pgClient, new Set(['0x10', '0x20']))
+
+      const { rows: created } = await pgClient.query(
+        'SELECT day::TEXT, participant_id FROM daily_participants'
+      )
+      assert.deepStrictEqual(created, [
+        { day: today, participant_id: 1 },
+        { day: today, participant_id: 2 }
+      ])
+    })
+
+    it('handles participants already seen today', async () => {
+      await updateDailyParticipants(pgClient, new Set(['0x10', '0x20']))
+      await updateDailyParticipants(pgClient, new Set(['0x10', '0x30', '0x20']))
+
+      const { rows: created } = await pgClient.query(
+        'SELECT day::TEXT, participant_id FROM daily_participants'
+      )
+      assert.deepStrictEqual(created, [
+        { day: today, participant_id: 1 },
+        { day: today, participant_id: 2 },
+        { day: today, participant_id: 3 }
+      ])
+    })
+
+    it('maps new participant addresses to new ids', async () => {
+      const ids = await mapParticipantsToIds(pgClient, new Set(['0x10', '0x20']))
+      ids.sort()
+      assert.deepStrictEqual(ids, [1, 2])
+    })
+
+    it('maps existing participants to their existing ids', async () => {
+      const participants = new Set(['0x10', '0x20'])
+      const first = await mapParticipantsToIds(pgClient, participants)
+      first.sort()
+      assert.deepStrictEqual(first, [1, 2])
+
+      participants.add('0x30')
+      participants.add('0x40')
+      const second = await mapParticipantsToIds(pgClient, participants)
+      second.sort()
+      assert.deepStrictEqual(second, [1, 2, 3, 4])
+    })
+  })
+
+  const getCurrentDate = async () => {
+    const { rows: [{ today }] } = await pgClient.query('SELECT now()::DATE::TEXT as today')
+    return today
+  }
 })
