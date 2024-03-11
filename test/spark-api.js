ad36c99b9821176675f559a6745ad5d6af23e49f
Julian Gruber <julian@juliangruber.com>
2024-03-11 11:23:41 +0100

    fix `spark-api` tests

--- test/spark-api.js
+++ test/spark-api.js
@@ -22,11 +22,13 @@ describe('spark-api client', () => {
     assert.deepStrictEqual(retrievalTasks.slice(0, 2), [
       {
         cid: 'Qmcieq8Lf1r4qfifJkKpdpVzajdsbSGZmBiL3vy6syWo5T',
+        minerId: null,
         protocol: 'graphsync',
         providerAddress: '/ip4/210.209.69.37/tcp/34568/p2p/12D3KooWSHG9vVStHMi9vhfgD1XaR221Ur6RjWcVsiY7WfVaX4QL'
       },
       {
         cid: 'QmejPgwMo5jzJDsArpGCn9Tz1gSQNXYU722pFe2kcLHzpq',
+        minerId: null,
         protocol: 'graphsync',
         providerAddress: '/ip4/138.113.222.200/tcp/19013/p2p/12D3KooWCnJeMnur6ScjXXUo8ptMKac88fDbD8GELmdDrdg3FBk5'
       }
