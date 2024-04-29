3cd709d9efc9b39be562c531dd3bde88de2f8f04
Miroslav Bajtoš <oss@bajtos.net>
2024-04-29 15:03:43 +0200

    ci: dry-run evaluation of the last Spark round (#196)
    * ci: dry-run evaluation of the last Spark round
    
    * fix: calculation of the last round index
    
    Calculate the last round as "currentRound - 2" to avoid the failures
    in case we run the code after a new round was started but before any
    measurements were committed.
    
    * feat: write stats to a real DB in dry-run
    
    * fix: remove noisy event log in dry-run
    
    * feat: crash dry-run on errors ignored in production
    
    In production, we consider the evaluation run as succesful even if
    we cannot produce some of the telemetry and stats.
    
    In the dry run, we want such errors to crash the process (and the CI
    build) so that we learn about the problem.
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- bin/dry-run.js
+++ bin/dry-run.js
@@ -1,7 +1,7 @@
 // dotenv must be imported before importing anything else
 import 'dotenv/config'
 
-import { IE_CONTRACT_ADDRESS, RPC_URL, rpcHeaders } from '../lib/config.js'
+import { DATABASE_URL, IE_CONTRACT_ADDRESS, RPC_URL, rpcHeaders } from '../lib/config.js'
 import { evaluate } from '../lib/evaluate.js'
 import { preprocess, fetchMeasurements } from '../lib/preprocess.js'
 import { fetchRoundDetails } from '../lib/spark-api.js'
@@ -10,6 +10,7 @@ import { readFile, writeFile, mkdir } from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { ethers } from 'ethers'
+import pg from 'pg'
 import { RoundData } from '../lib/round.js'
 
 const cacheDir = fileURLToPath(new URL('../.cache', import.meta.url))
@@ -38,7 +39,7 @@ if (roundIndexStr) {
 } else {
   console.log('Round index not specified, fetching the last round index from the smart contract')
   const currentRoundIndex = await fetchLastRoundIndex()
-  roundIndex = Number(currentRoundIndex - 1n)
+  roundIndex = Number(currentRoundIndex - 2n)
 }
 
 if (!measurementCids.length) {
@@ -54,6 +55,12 @@ const recordTelemetry = (measurementName, fn) => {
   const point = new Point(measurementName)
   fn(point)
   console.log('TELEMETRY %s %o', measurementName, point.fields)
+}
+
+const createPgClient = async () => {
+  const pgClient = new pg.Client({ connectionString: DATABASE_URL })
+  await pgClient.connect()
+  return pgClient
 }
 
 const fetchMeasurementsWithCache = async (cid) => {
@@ -100,23 +107,28 @@ const ieContractWithSigner = {
   }
 }
 
-await evaluate({
+const started = Date.now()
+const { ignoredErrors } = await evaluate({
   roundIndex,
   round,
   fetchRoundDetails,
   ieContractWithSigner,
   logger: console,
   recordTelemetry,
-
-  // We don't want dry runs to update data in `sparks_stats`, therefore we are passing a stub
-  // connection factory that creates no-op clients. This also keeps the setup simpler. The person
-  // executing a dry run does not need access to any Postgres instance.
-  // Evaluate uses the PG client only for updating the statistics, it's not reading any data.
-  // Thus it's safe to inject a no-op client.
-  createPgClient: createNoopPgClient
+  createPgClient
 })
 
+console.log('Duration: %sms', Date.now() - started)
 console.log(process.memoryUsage())
+
+if (ignoredErrors.length) {
+  console.log('**ERRORS**')
+  for (const err of ignoredErrors) {
+    console.log()
+    console.log(err)
+  }
+  process.exit(1)
+}
 
 async function fetchMeasurementsAddedEvents (roundIndex) {
   const pathOfCachedResponse = path.join(cacheDir, 'round-' + roundIndex + '.json')
@@ -175,7 +187,7 @@ async function fetchMeasurementsAddedFromChain (roundIndex) {
 
   /** @type {Array<{ cid: string, roundIndex: bigint, sender: string }>} */
   const events = rawEvents.map(({ args: [cid, roundIndex, sender] }) => ({ cid, roundIndex, sender }))
-  console.log('events', events)
+  // console.log('events', events)
 
   const prev = roundIndex - 1n
   const prevFound = events.some(e => e.roundIndex === prev)
@@ -203,15 +215,4 @@ async function fetchMeasurementsAddedFromChain (roundIndex) {
 async function fetchLastRoundIndex () {
   const { ieContract } = await createIeContract()
   return await ieContract.currentRoundIndex()
-}
-
-function createNoopPgClient () {
-  return {
-    async query () {
-      return { rows: [] }
-    },
-    async end () {
-      // no-op
-    }
-  }
 }
