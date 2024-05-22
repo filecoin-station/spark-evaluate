filecoin-station/spark-evaluate

commit 0b7f0cddf56f125f60babb7fa827a7885acc9258

fix: delay evaluation to finish preprocessing (#224)
A temporary hacky fix for #64

Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

index.js
@@ -8,6 +8,8 @@ import timers from 'node:timers/promises'
 // Tweak this value to improve the chances of the data being available
 const PREPROCESS_DELAY = 60_000
 
+const EVALUATE_DELAY = PREPROCESS_DELAY + 60_000
+
 export const startEvaluate = async ({
   ieContract,
   ieContractWithSigner,
@@ -67,7 +69,7 @@ export const startEvaluate = async ({
     }
   }
 
-  const onRoundStart = (_roundIndex) => {
+  const onRoundStart = async (_roundIndex) => {
     const roundIndex = BigInt(_roundIndex)
     if (roundsSeen.includes(roundIndex)) return
     roundsSeen.push(roundIndex)
@@ -79,6 +81,13 @@ export const startEvaluate = async ({
       console.error('No current round data available, skipping evaluation')
       return
     }
+
+    // TODO: Fix this properly and implement a signalling mechanism allowing the "preprocess" step
+    // to notify the "evaluate" when the preprocessing is done, so that we don't have to use a timer
+    // here. See also https://github.com/filecoin-station/spark-evaluate/issues/64
+    console.log(`Sleeping for ${EVALUATE_DELAY}ms before evaluating the round to let the preprocess step finish for the last batch of measurements`)
+    await timers.setTimeout(EVALUATE_DELAY)
+    console.log(`Now evaluating the round ${roundIndex}`)
 
     rounds.previous = rounds.current
     rounds.current = new RoundData(roundIndex)
@@ -109,5 +118,11 @@ export const startEvaluate = async ({
       Sentry.captureException(err)
     })
   })
-  ieContract.on('RoundStart', onRoundStart)
+
+  ieContract.on('RoundStart', (...args) => {
+    onRoundStart(...args).catch(err => {
+      console.error(err)
+      Sentry.captureException(err)
+    })
+  })
 }

commit b178c2dc0809feb2a37d4c6e2f9fd64b33799442

fix: advance round before waiting for preprocess
Fix the regression introduced by #224 where the first measurements batch
added to a new round triggers the following error:

```
Round index mismatch: 7386 !== 7387
```

Sentry issue:
https://space-meridian.sentry.io/issues/5385606165/?project=4505906069766144

Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

index.js
@@ -82,15 +82,15 @@ export const startEvaluate = async ({
       return
     }
 
+    rounds.previous = rounds.current
+    rounds.current = new RoundData(roundIndex)
+
     // TODO: Fix this properly and implement a signalling mechanism allowing the "preprocess" step
     // to notify the "evaluate" when the preprocessing is done, so that we don't have to use a timer
     // here. See also https://github.com/filecoin-station/spark-evaluate/issues/64
     console.log(`Sleeping for ${EVALUATE_DELAY}ms before evaluating the round to let the preprocess step finish for the last batch of measurements`)
     await timers.setTimeout(EVALUATE_DELAY)
     console.log(`Now evaluating the round ${roundIndex}`)
-
-    rounds.previous = rounds.current
-    rounds.current = new RoundData(roundIndex)
 
     // Evaluate previous round
     evaluate({