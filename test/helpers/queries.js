4b6b8010224acc30312d744f7c28c85b2e679e05
Julian Gruber <julian@juliangruber.com>
2024-06-12 15:13:11 +0200

    add types, fix lint

--- test/helpers/queries.js
+++ test/helpers/queries.js
@@ -1,15 +1,20 @@
 import { mapParticipantsToIds } from '../../lib/platform-stats.js'
 
-// Used by spark-stats
-// import { givenDailyParticipants } from 'spark-evaluate/test/helpers/queries'
-export const givenDailyParticipants = async (pgPool, day, participantAddresses) => { 
-  const ids = await mapParticipantsToIds(pgPool, new Set(participantAddresses)) 
-  await pgPool.query(` 
-    INSERT INTO daily_participants (day, participant_id) 
-    SELECT $1 as day, UNNEST($2::INT[]) AS participant_id 
-    ON CONFLICT DO NOTHING 
-  `, [ 
-    day, 
-    ids 
-  ]) 
+/**
+ * Used by spark-stats
+ * import { givenDailyParticipants } from 'spark-evaluate/test/helpers/queries'
+ * @param {import('pg').Client} pgClient
+ * @param {string} day
+ * @param {string} participantAddresses
+ */
+export const givenDailyParticipants = async (pgClient, day, participantAddresses) => {
+  const ids = await mapParticipantsToIds(pgClient, new Set(participantAddresses))
+  await pgClient.query(`
+    INSERT INTO daily_participants (day, participant_id)
+    SELECT $1 as day, UNNEST($2::INT[]) AS participant_id
+    ON CONFLICT DO NOTHING
+  `, [
+    day,
+    ids
+  ])
 }
