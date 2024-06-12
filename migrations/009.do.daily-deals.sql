9e175400652623f19569eecfd97706a77084ff36
Miroslav Bajtoš <oss@bajtos.net>
2024-06-12 17:17:02 +0200

    feat: daily deals stats: total/indexed/retrievable (#256)
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

---
+++ migrations/009.do.daily-deals.sql
@@ -0,0 +1,6 @@
+CREATE TABLE daily_deals (
+  day DATE NOT NULL PRIMARY KEY,
+  total INT NOT NULL,
+  indexed INT NOT NULL,
+  retrievable INT NOT NULL
+);
--- test/public-stats.test.js
+++ test/public-stats.test.js
@@ -26,6 +26,7 @@ describe('public-stats', () => {
   beforeEach(async () => {
     await pgClient.query('DELETE FROM retrieval_stats')
     await pgClient.query('DELETE FROM indexer_query_stats')
+    await pgClient.query('DELETE FROM daily_deals')
 
     // Run all tests inside a transaction to ensure `now()` always returns the same value
     // See https://dba.stackexchange.com/a/63549/125312
@@ -134,6 +135,47 @@ describe('public-stats', () => {
       assert.deepStrictEqual(updated, [
         { day: today, deals_tested: 3 + 4, deals_advertising_http: 1 + 1 }
       ])
+    })
+  })
+
+  describe('daily_deals', () => {
+    it('creates or updates the row for today', async () => {
+      /** @type {Measurement[]} */
+      const honestMeasurements = [
+        { ...VALID_MEASUREMENT },
+        // HTTP_NOT_ADVERTISED means the deal is indexed
+        { ...VALID_MEASUREMENT, cid: 'bafy2', indexerResult: 'HTTP_NOT_ADVERTISED', retrievalResult: 'IPNI_HTTP_NOT_ADVERTISED' },
+        { ...VALID_MEASUREMENT, cid: 'bafy3', indexerResult: 'ERROR_404', retrievalResult: 'IPNI_ERROR_404' },
+        { ...VALID_MEASUREMENT, cid: 'bafy4', status_code: 502, retrievalResult: 'ERROR_502' }
+      ]
+      const allMeasurements = honestMeasurements
+      await updatePublicStats({ createPgClient, honestMeasurements, allMeasurements })
+
+      const { rows: created } = await pgClient.query(
+        'SELECT day::TEXT, total, indexed, retrievable FROM daily_deals'
+      )
+      assert.deepStrictEqual(created, [
+        { day: today, total: 4, indexed: 3, retrievable: 1 }
+      ])
+
+      // Notice: this measurement is for the same task as honestMeasurements[0], therefore it's
+      // effectively ignored as the other measurement was successful.
+      honestMeasurements.push({ ...VALID_MEASUREMENT, status_code: 502 })
+      // These are measurements for a new task.
+      honestMeasurements.push({ ...VALID_MEASUREMENT, cid: 'bafy5', indexerResult: 'UNKNOWN_ERROR', retrievalResult: 'IPNI_UNKNOWN_ERROR' })
+      honestMeasurements.push({ ...VALID_MEASUREMENT, cid: 'bafy5', status_code: 502, retrievalResult: 'ERROR_502' })
+
+      await updatePublicStats({ createPgClient, honestMeasurements, allMeasurements })
+
+      const { rows: updated } = await pgClient.query(
+        'SELECT day::TEXT, total, indexed, retrievable FROM daily_deals'
+      )
+      assert.deepStrictEqual(updated, [{
+        day: today,
+        total: 2 * 4 + 1 /* added bafy5 */,
+        indexed: 2 * 3 + 1 /* bafy5 is indexed */,
+        retrievable: 2 * 1 + 0 /* bafy5 not retrievable */
+      }])
     })
   })
 
