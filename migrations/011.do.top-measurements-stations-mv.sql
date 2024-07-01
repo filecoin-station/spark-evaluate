4a9bf217089add24428093a8b842816051a02e42
PatrickNercessian <pjnercessian@gmail.com>
2024-07-01 10:55:24 -0400

    feat: add materialized view for top stations by measurement count

---
+++ migrations/011.do.top-measurements-stations-mv.sql
@@ -0,0 +1,10 @@
+CREATE MATERIALIZED VIEW top_measurement_stations_mv AS
+SELECT
+  participant_address,
+  COUNT(DISTINCT inet_group) AS inet_group_count,
+  COUNT(DISTINCT station_id) AS station_count,
+  SUM(accepted_measurement_count) AS accepted_measurement_count
+FROM daily_stations
+WHERE day = CURRENT_DATE - INTERVAL '1 day'
+GROUP BY participant_address
+ORDER BY accepted_measurement_count DESC;
