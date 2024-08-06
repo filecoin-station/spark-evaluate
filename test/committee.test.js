0732097a3c8a1b1510edafdc45b3ff5663071f13
Miroslav Bajtoš <oss@bajtos.net>
2024-08-06 15:56:22 +0200

    refactor: committee.measurements are private now
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/committee.test.js
+++ test/committee.test.js
@@ -15,16 +15,15 @@ describe('Committee', () => {
   describe('evaluate', () => {
     it('produces OK result when the absolute majority agrees', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
       // minority result
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'CONTENT_VERIFICATION_FAILED' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'CONTENT_VERIFICATION_FAILED' })
 
       c.evaluate(2)
 
+      assert.strictEqual(c.indexerResult, 'OK')
       assert.strictEqual(c.retrievalResult, 'OK')
-      // TODO, we are not there yet
-      // assert.strictEqual(c.indexerResult, 'OK')
       assert.deepStrictEqual(c.measurements.map(m => m.fraudAssessment), [
         'OK',
         'OK',
@@ -34,7 +33,7 @@ describe('Committee', () => {
 
     it('rejects committees that are too small', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT })
+      c.addMeasurement({ ...VALID_MEASUREMENT })
       c.evaluate(10)
       assert.strictEqual(c.retrievalResult, 'COMMITTEE_TOO_SMALL')
       assert.strictEqual(c.measurements[0].fraudAssessment, 'COMMITTEE_TOO_SMALL')
@@ -42,9 +41,9 @@ describe('Committee', () => {
 
     it('rejects committees without absolute majority for providerId', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, providerId: 'pubkey1' })
-      c.measurements.push({ ...VALID_MEASUREMENT, providerId: 'pubkey2' })
-      c.measurements.push({ ...VALID_MEASUREMENT, providerId: 'pubkey3' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, providerId: 'pubkey1' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, providerId: 'pubkey2' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, providerId: 'pubkey3' })
 
       c.evaluate(2)
 
@@ -58,10 +57,10 @@ describe('Committee', () => {
 
     it('finds majority for providerId', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, providerId: 'pubkey1' })
-      c.measurements.push({ ...VALID_MEASUREMENT, providerId: 'pubkey1' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, providerId: 'pubkey1' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, providerId: 'pubkey1' })
       // minority result
-      c.measurements.push({ ...VALID_MEASUREMENT, providerId: 'pubkey3' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, providerId: 'pubkey3' })
 
       c.evaluate(2)
 
@@ -75,9 +74,9 @@ describe('Committee', () => {
 
     it('rejects committees without absolute majority for retrievalResult', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'IPNI_ERROR_404' })
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'ERROR_502' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'IPNI_ERROR_404' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'ERROR_502' })
 
       c.evaluate(2)
 
@@ -91,10 +90,10 @@ describe('Committee', () => {
 
     it('finds majority for retrievalResult', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'CONTENT_VERIFICATION_FAILED' })
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'CONTENT_VERIFICATION_FAILED' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'CONTENT_VERIFICATION_FAILED' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'CONTENT_VERIFICATION_FAILED' })
       // minority result
-      c.measurements.push({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, retrievalResult: 'OK' })
 
       c.evaluate(2)
 
@@ -108,9 +107,9 @@ describe('Committee', () => {
 
     it('rejects committees without absolute majority for indexerResult', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, indexerResult: 'OK' })
-      c.measurements.push({ ...VALID_MEASUREMENT, indexerResult: 'ERROR_404' })
-      c.measurements.push({ ...VALID_MEASUREMENT, indexerResult: 'HTTP_NOT_ADVERTISED' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, indexerResult: 'OK' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, indexerResult: 'ERROR_404' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, indexerResult: 'HTTP_NOT_ADVERTISED' })
 
       c.evaluate(2)
 
@@ -125,10 +124,10 @@ describe('Committee', () => {
 
     it('finds majority for indexerResult', () => {
       const c = new Committee(VALID_TASK)
-      c.measurements.push({ ...VALID_MEASUREMENT, indexerResult: 'HTTP_NOT_ADVERTISED' })
-      c.measurements.push({ ...VALID_MEASUREMENT, indexerResult: 'HTTP_NOT_ADVERTISED' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, indexerResult: 'HTTP_NOT_ADVERTISED' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, indexerResult: 'HTTP_NOT_ADVERTISED' })
       // minority result
-      c.measurements.push({ ...VALID_MEASUREMENT, indexerResult: 'OK' })
+      c.addMeasurement({ ...VALID_MEASUREMENT, indexerResult: 'OK' })
 
       c.evaluate(2)
 
