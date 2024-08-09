0908cf4e1f882fc649a0a9dcdfdaeef44e1f3fc3
Miroslav Bajtoš <oss@bajtos.net>
2024-08-09 15:11:42 +0200

    feat: find the TX message in the mempool
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/cancel-pending-tx.js
+++ bin/cancel-pending-tx.js
@@ -2,6 +2,7 @@
 import 'dotenv/config'
 
 import assert from 'node:assert'
+import { RPC_URL, rpcHeaders } from '../lib/config.js'
 import { ethers } from 'ethers'
 import { CoinType, newDelegatedEthAddress } from '@glif/filecoin-address'
 
@@ -19,16 +20,48 @@ assert(tx, 'Transaction hash must be provided as the first argument')
 const { ieContract, provider } = await createMeridianContract()
 
 const signer = ethers.Wallet.fromPhrase(WALLET_SEED, provider)
+const walletDelegatedAddress = newDelegatedEthAddress(/** @type {any} */(signer.address), CoinType.MAIN).toString()
 console.log(
   'Wallet address:',
   signer.address,
-  newDelegatedEthAddress(/** @type {any} */(signer.address), CoinType.MAIN).toString()
+  walletDelegatedAddress
 )
 const ieContractWithSigner = ieContract.connect(signer)
 
 console.log('Going to revoke %s', tx)
 
+const messages = await getMessagesInMempool(walletDelegatedAddress)
+const txDetails = messages.find(m => m.cid === tx)
+if (!txDetails) {
+  console.log('Transaction message not found in the mempool.')
+  console.log('https://filfox.info/en/message/%s', tx)
+  console.log('Note: we have access to 50 oldest messages only.')
+  for (const { cid, createTimestamp } of messages) {
+    console.log(' %s (created at %s)', cid, new Date(createTimestamp * 1000).toISOString())
+  }
+  process.exit(1)
+}
+console.log('TX message:', txDetails)
+/* example data:
+TX message: {
+  cid: 'bafy2bzacecblagyenhtbrri4vwslq4ybv42nqmu7j7ltagfd2sefg4sne72eo',
+  from: 'f410fj3g4re56wcisdzhvzo5enhjt6x7wdbccfitupvq',
+  to: 'f410fqrqhm3w4mk2sl7a7utlcr7dzeko4ombrjptgwui',
+  nonce: 74574,
+  value: '0',
+  gasLimit: 4704651802,
+  gasFeeCap: '368268',
+  gasPremium: '233112',
+  method: 'InvokeContract',
+  methodNumber: 3844450837,
+  evmMethod: '',
+  createTimestamp: 1723077205
+}
+*/
+
 /*
+TODO:
+
 https://filecoinproject.slack.com/archives/C0179RNEMU4/p1607269156412600?thread_ts=1606987775.021900&cid=C0179RNEMU4
 while you can not cancel a message, you have an option to replace it with a different one, in this
 case you would replace it with the cheapest possible one ( a self-send of 0 ) in order to "convince"
@@ -56,3 +89,71 @@ lotus send
 that should be all, and should not cost you much at all ( we just cleared ~80 messages in the thread
 above by paying ~0.05 fil
  */
+
+/**
+ * @param {string} f4addr
+ * @returns 50 oldest messages
+ */
+async function getMessagesInMempool (f4addr) {
+  const res = await fetch(`https://filfox.info/api/v1/message/mempool/filtered-list?address=${f4addr}`)
+  if (!res.ok) {
+    throw new Error(`Filfox request failed with ${res.status}: ${(await res.text()).trimEnd()}`)
+  }
+  /** @type {{
+     messages: {
+       cid: string;
+       from: string;
+       to: string;
+       nonce: number;
+       value: string;
+       gasLimit: number;
+       gasFeeCap: string;
+       gasPremium: string;
+       method: string;
+       methodNumber: number;
+       evmMethod: string;
+       createTimestamp: number;
+     }[];
+   }}
+   */
+  const { messages } = /** @type {any} */(await res.json())
+  return messages
+}
+
+/**
+ * @param {string} method
+ * @param {unknown[]} params
+ */
+async function rpc (method, ...params) {
+  const req = new Request(RPC_URL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accepts: 'application/json',
+      ...rpcHeaders
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      id: 1,
+      method,
+      params
+    })
+  })
+  const res = await fetch(req, {
+    signal: AbortSignal.timeout(60_000)
+  })
+
+  if (!res.ok) {
+    throw new Error(`JSON RPC failed with ${res.status}: ${(await res.text()).trimEnd()}`)
+  }
+
+  const body = /** @type {any} */(await res.json())
+  if (body.error) {
+    const err = new Error(body.error.message)
+    err.name = 'FilecoinRpcError'
+    Object.assign(err, { code: body.code })
+    throw err
+  }
+
+  return body.result
+}
