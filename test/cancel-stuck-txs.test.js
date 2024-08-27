cbd6f1096e9adad860e2f3debc1eccd1ee954123
Julian Gruber <julian@juliangruber.com>
2024-08-27 16:22:17 +0200

    improve tests

--- test/cancel-stuck-txs.test.js
+++ test/cancel-stuck-txs.test.js
@@ -4,7 +4,9 @@ import {
   getRecentSendMessage
 } from '../lib/cancel-stuck-txs.js'
 
-describe('cancel stuck transactions', () => {
+describe('cancel stuck transactions', function () {
+  this.timeout(10_000)
+
   describe('getMessagesInMempool(addr)', () => {
     it('should return messages in mempool', async () => {
       const messages = await getMessagesInMempool(
@@ -16,7 +18,8 @@ describe('cancel stuck transactions', () => {
   describe('getRecentSendMessage()', () => {
     it('should return a recent send message', async () => {
       const recentSendMessage = await getRecentSendMessage()
-      assert(recentSendMessage)
+      assert(recentSendMessage.receipt.gasUsed)
+      assert(recentSendMessage.gasFeeCap)
     })
   })
 })
