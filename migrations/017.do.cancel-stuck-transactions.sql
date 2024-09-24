72c03e48df9c855ca883290b6405e2ee8371b5cf
Julian Gruber <julian@juliangruber.com>
2024-09-24 13:57:22 +0200

    text -> timestamptz

--- migrations/017.do.cancel-stuck-transactions.sql
+++ migrations/017.do.cancel-stuck-transactions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE transactions_pending (
   hash TEXT NOT NULL PRIMARY KEY,
-  timestamp TEXT NOT NULL,
+  timestamp TIMESTAMPTZ NOT NULL,
   from_address TEXT NOT NULL,
   max_priority_fee_per_gas BIGINT NOT NULL,
   nonce INT NOT NULL
