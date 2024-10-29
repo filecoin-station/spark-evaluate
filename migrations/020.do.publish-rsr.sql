cd8aa543e3cbca20213b297c7a633bb81bcbc583
Julian Gruber <julian@juliangruber.com>
2024-10-29 11:40:36 +0100

    measurement_commitments -> _batches

--- migrations/020.do.publish-rsr.sql
+++ migrations/020.do.publish-rsr.sql
@@ -3,7 +3,7 @@ CREATE TABLE unpublished_rsr_rounds (
   contract_address TEXT NOT NULL,
   evaluated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   spark_evaluate_version TEXT NOT NULL,
-  measurement_commitments TEXT[] NOT NULL,
+  measurement_batches TEXT[] NOT NULL,
   round_details JSONB NOT NULL,
   provider_retrieval_result_stats JSONB NOT NULL,
   PRIMARY KEY (round_index, contract_address)
