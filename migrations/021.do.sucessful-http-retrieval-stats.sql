bb351e646e08366ec0a25c0950f5e568a388cee2
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2025-01-08 16:14:04 +0100

    remove NL

--- migrations/021.do.sucessful-http-retrieval-stats.sql
+++ migrations/021.do.sucessful-http-retrieval-stats.sql
@@ -1,2 +1,1 @@
-ALTER TABLE retrieval_stats
-ADD COLUMN IF NOT EXISTS successful_http INT NOT NULL DEFAULT 0;
+ALTER TABLE retrieval_stats ADD COLUMN IF NOT EXISTS successful_http INT NOT NULL DEFAULT 0;
